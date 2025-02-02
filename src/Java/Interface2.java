c6646e5e298becdc1665bd9cd0954f5f65ce0855
2kavitachaubey <chaubeykavita750@gmail.com>
2025-02-02 23:59:25 +0530

    Basic Java

---
+++ src/Java/Interface2.java
@@ -0,0 +1,28 @@
+package Java;
+interface Computer{
+    void process();
+}
+class Laptop implements Computer{
+    public void process(){
+        System.out.println("Run, Compile, Run");
+    }
+}
+class Desktop implements Computer{
+    public void process(){
+        System.out.println("Run, Compile, Run, Faster");
+    }
+}
+class Developer{
+    public void desk(Computer lap){
+        lap.process();
+    }
+}
+public class Interface2 {
+    public static void main(String[] args) {
+        Computer dk = new Desktop();
+        Computer lap = new Laptop();
+
+        Developer dev = new Developer();
+        dev.desk(dk);
+    }
+}
