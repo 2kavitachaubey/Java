05c1849c96d50762ea842d03005281e14531cf5e
2kavitachaubey <chaubeykavita750@gmail.com>
2025-02-07 23:49:32 +0530

    Basic Java

---
+++ src/Java/SwingCalc.java
@@ -0,0 +1,81 @@
+package Java;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class SwingCalc extends JFrame{
+    SwingCalc(){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(400,400);
+        setLayout(null);
+
+        JTextField number1, number2, result;
+        JButton b1, b2;
+
+        JLabel num1 = new JLabel("Enter First Number: ");
+        num1.setBounds(50,20,150,30);
+        JLabel num2 = new JLabel("Enter Second Number: ");
+        num2.setBounds(50,50,150,30);
+        JLabel r1 = new JLabel("Result: ");
+        r1.setBounds(50, 80, 150, 30);
+
+        number1 = new JTextField();
+        number1.setBounds(200,20,100,20);
+        number2 = new JTextField();
+        number2.setBounds(200,60,100,20);
+        result = new JTextField();
+        result.setBounds(200,90,100,20);
+
+        b1 = new JButton("+");
+        b1.setBounds(50,150,50,50);
+        b2 = new JButton("/");
+        b2.setBounds(100,150,50,50);
+
+        add(num1);
+        add(num2);
+        add(r1);
+        add(number1);
+        add(number2);
+        add(result);
+        add(b1);
+        add(b2);
+
+        b1.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent ex){
+                double a = Double.parseDouble(number1.getText());
+                double b = Double.parseDouble(number2.getText());
+                double c = 0;
+                if(ex.getSource() == b1){
+                    c=a+b;
+                }
+                result.setText(String.valueOf(c));
+            }
+        });
+
+        b2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double a = Double.parseDouble(number1.getText());
+                double b = Double.parseDouble(number2.getText());
+                double c = 0;
+                if(e.getSource() == b2){
+                    try{
+                        if(e.getSource() == b2){
+                        c = a /b;}
+                    }catch (Exception ex){
+                        JOptionPane.showMessageDialog(null,"Can't divide by zero...");
+                        System.out.println(0);
+                    }
+                }
+                result.setText(String.valueOf(c));
+            }
+        });
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new SwingCalc();
+    }
+}
---
+++ src/Java/SwingExample2.java
@@ -0,0 +1,62 @@
+package Java;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+
+public class SwingExample2 extends JFrame implements ActionListener {
+    JTextField lengthField, widthField, resultField;
+    JButton calculateButton;
+    SwingExample2(){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setSize(400,400);
+        setLayout(null);
+
+        JLabel l = new JLabel("length: ");
+        l.setBounds(20, 20, 60, 20);
+        JLabel widthLabel = new JLabel("Width:");
+        widthLabel.setBounds(20, 50, 60, 20);
+        JLabel resultLabel = new JLabel("Area:");
+        resultLabel.setBounds(20, 80, 60, 20);
+
+        lengthField = new JTextField();
+        lengthField.setBounds(90, 20, 150, 20);
+        widthField = new JTextField();
+        widthField.setBounds(90, 50, 150, 20);
+        resultField = new JTextField();
+        resultField.setBounds(90, 80, 150, 20);
+        resultField.setEditable(false);
+
+        calculateButton = new JButton("Calculate");
+        calculateButton.setBounds(90, 110, 100, 30);
+        calculateButton.addActionListener(this);
+
+        add(l);
+        add(widthLabel);
+        add(resultLabel);
+        add(lengthField);
+        add(widthField);
+        add(resultField);
+        add(calculateButton);
+
+        setVisible(true);
+    }
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource() == calculateButton){
+            try{
+                double length = Double.parseDouble(lengthField.getText());
+                double width = Double.parseDouble(widthField.getText());
+                double area = length*width;
+
+                resultField.setText(String.format("%.2f", area));
+            }catch (NumberFormatException ex){
+                JOptionPane.showMessageDialog(null, "kjagkasga");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+      new SwingExample2();
+    }
+}
