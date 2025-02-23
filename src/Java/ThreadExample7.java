bca294de0734b1476b5a37a1c6333073f2491c7b
2kavitachaubey <chaubeykavita750@gmail.com>
2025-02-23 12:16:02 +0530

    Java Even Odd Project

---
+++ src/Java/ThreadExample7.java
@@ -0,0 +1,18 @@
+package Java;
+
+import java.lang.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadExample7 implements Runnable{
+    public void run(){
+        System.out.println(Thread.currentThread().getName());
+    }
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(10);
+        for(int i = 1; i<=100; i++){
+            service.execute(new ThreadExample7());
+        }
+        System.out.println(Thread.currentThread().getName());
+    }
+}
