03d2f383df55bbc4b5f20f34d3ab0f5813ab69b0
2kavitachaubey <chaubeykavita750@gmail.com>
2025-03-04 08:18:16 +0530

    Java Even Odd Project

---
+++ src/CoreJava/VariableNonPremitive.java
@@ -0,0 +1,15 @@
+package CoreJava;
+
+import java.util.Date;
+
+public class VariableNonPremitive {
+    public static void main(String[] args) {
+        String str = "Kavita";
+        //String is a class that store a string variable...
+        //Every class store a reference type
+        Date date = new Date();
+        // new keyword use to allocate the memory
+        System.out.println(str);
+        System.out.println(date);
+    }
+}
